c80a05fd5067d1b7f7657f8cc0a2f2d2d846f4d5
DFCH9503 <dafechaparro@hotmail.com>
2025-03-13 11:01:00 -0500

    solving palindrome number go two pointer approach

--- go/palindrome-number/palindrome-number.go
+++ go/palindrome-number/palindrome-number.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+    "math" //comment for brute approach
 )
 
 func toStr(x int)string{
@@ -10,20 +11,42 @@ func toStr(x int)string{
     return xStr
 }
 
-func reverse(str string) (result string) { 
-    for _, v := range str { 
-        result = string(v) + result 
-    } 
-    return
-} 
+//uncomment for brute approach 
+// func reverse(str string) (result string) { 
+//     for _, v := range str { 
+//         result = string(v) + result 
+//     } 
+//     return
+// } 
+
 
 func isPalindrome(x int) bool {
-    xStr := toStr(x)
-	xRev := reverse(xStr)
-	return xStr == xRev
+
+    //brute approach
+    // xStr := toStr(x)
+	// xRev := reverse(xStr)
+	// return xStr == xRev
+
+    //using other approach, two pointer
+    left , right := 0 , len(toStr(x))
+    xString := toStr(x)
+    xRune := []rune(xString)
+    fmt.Println(xRune)
+    for i := left; i< int(math.Floor(float64(right)/2)); i++ {
+        charRight := xRune[right-(i+1)]
+        charLeft := xRune[i]
+        if charLeft == charRight{
+            continue
+        }else {
+            return false
+        }
+    }
+    return true
 }
 
+
 func main(){
-	isPalindrome(121)
-	fmt.Println("answer to the first example is:",isPalindrome(121))
+    fmt.Println("answer to the first example is:",isPalindrome(121))
 }
+
+
